4a5f1b989f7a439431a03973ab01a7c8d73cb793
Maxwell Foley <maxwellsfoley@gmail.com>
2026-01-16 23:18:20 -0500

    animate header on load

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,5 +16,11 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/typography'),
+    require('taos/plugin'),
   ],
+  safelist: [
+    '!duration-[0ms]',
+    '!delay-[0ms]',
+    'html.js :where([class*="taos:"]:not(.taos-init))',
+  ]
 } 
